cd42b891338d62f5965e102f61b5c2cb52637ba1
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-09-07 15:16:53 +0300

    Added Lab2 src and doc

---
+++ Lab2/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"app","l":"app()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"app","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
